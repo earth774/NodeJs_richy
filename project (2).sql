fcc205bb0ca6625c0027dfb961d63ecdd95efac3
57022095 <57022095@live.up.ac.th>
2017-12-28 14:24:28 +0700

    test

---
+++ project (2).sql
@@ -0,0 +1,224 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 28, 2017 at 08:24 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `amphur`
+--
+
+CREATE TABLE `amphur` (
+  `AMPHUR_ID` int(5) NOT NULL,
+  `AMPHUR_CODE` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
+  `AMPHUR_NAME` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `AMPHUR_NAME_ENG` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `GEO_ID` int(5) NOT NULL DEFAULT '0',
+  `PROVINCE_ID` int(5) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `order_total` int(11) NOT NULL,
+  `order_num` int(11) NOT NULL,
+  `order_status` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_description`
+--
+
+CREATE TABLE `order_description` (
+  `order_d_id` int(11) NOT NULL,
+  `order_d_name` varchar(100) NOT NULL,
+  `order_d_img` varchar(255) NOT NULL,
+  `order_d_title` varchar(255) NOT NULL,
+  `order_d_qty` int(11) NOT NULL,
+  `order_d_price` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_goods`
+--
+
+CREATE TABLE `order_goods` (
+  `og_id` int(11) NOT NULL,
+  `og_name` varchar(255) NOT NULL,
+  `og_pic` varchar(255) NOT NULL,
+  `og_price` int(11) NOT NULL,
+  `og_des` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `province`
+--
+
+CREATE TABLE `province` (
+  `PROVINCE_ID` int(5) NOT NULL,
+  `PROVINCE_CODE` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
+  `PROVINCE_NAME` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `PROVINCE_NAME_ENG` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `GEO_ID` int(5) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL,
+  `status_type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_image` varchar(255) NOT NULL,
+  `user_username` varchar(100) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `user_head` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_tel` varchar(12) NOT NULL,
+  `user_facebook` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_address` text NOT NULL,
+  `user_province` varchar(255) NOT NULL,
+  `user_amphur` varchar(100) NOT NULL,
+  `user_lat` varchar(20) NOT NULL,
+  `user_lng` varchar(20) NOT NULL,
+  `user_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `amphur`
+--
+ALTER TABLE `amphur`
+  ADD PRIMARY KEY (`AMPHUR_ID`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_description`
+--
+ALTER TABLE `order_description`
+  ADD PRIMARY KEY (`order_d_id`);
+
+--
+-- Indexes for table `order_goods`
+--
+ALTER TABLE `order_goods`
+  ADD PRIMARY KEY (`og_id`);
+
+--
+-- Indexes for table `province`
+--
+ALTER TABLE `province`
+  ADD PRIMARY KEY (`PROVINCE_ID`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_username` (`user_username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `amphur`
+--
+ALTER TABLE `amphur`
+  MODIFY `AMPHUR_ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=999;
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+--
+-- AUTO_INCREMENT for table `order_description`
+--
+ALTER TABLE `order_description`
+  MODIFY `order_d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=184;
+--
+-- AUTO_INCREMENT for table `order_goods`
+--
+ALTER TABLE `order_goods`
+  MODIFY `og_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `province`
+--
+ALTER TABLE `province`
+  MODIFY `PROVINCE_ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
