d03493c4a4efd1886a643cae9aa6e0bba76004d5
57022095 <57022095@live.up.ac.th>
2017-12-28 14:14:22 +0700

    test

--- controllers/contro.js
+++ controllers/contro.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     
     app.get('/api',function (req,res) {
         var url = req.protocol + '://' + req.get('host') ; 
-        res.json({ firstname:'John', lastname:'Doe',image:'http://localhost:3000/picture/order/20171107170320.jpg'.substring(url.length) });
+        res.json({ firstname:'John', lastname:'Doe',image:url+'picture/order/20171107170320.jpg'.substring(url.length) });
     });
 
     app.get('/time',function (req,res) {
